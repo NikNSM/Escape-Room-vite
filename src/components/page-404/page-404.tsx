d15248056c4b9a4eb970abddde87792be8670b47
Никита Межецкий <nsm020598@gmail.com>
2024-09-02 13:57:44 +1000

    Добавит перенаправление на главную страницу

--- src/components/page-404/page-404.tsx
+++ src/components/page-404/page-404.tsx
@@ -1,12 +1,15 @@
+import { Link } from 'react-router-dom';
+import { RouteAdresses } from '../../const';
+
 export default function Page404(): JSX.Element {
   return (
     <main>
       <div style={{ textAlign: 'center' }}>
         <p className="title--size-l title--uppercase">404</p>
         <p className="title--size-s">Кажется вы заблудился. Вернемся в начало?</p>
-        <a className="btn btn--accent" href="#">
+        <Link className="btn btn--accent" to={RouteAdresses.MAIN_PAGE}>
           <p className="title--size-s">На Главную</p>
-        </a>
+        </Link>
       </div>
     </main>
   );
